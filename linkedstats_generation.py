f43a37ea6b5e2e6beea8c7aef56719783ad533b0
Diego Conde <diegocondeherreros@gmail.com>
2024-07-24 10:43:54 +0200

    Script de generación automática añadido

--- linkedstats_generation.py
+++ linkedstats_generation.py
@@ -1,17 +1,15 @@
-# myscript.py
-
-import cx_Oracle
-
-# Connect as user "hr" with password "welcome" to the "orclpdb1" service running on this computer.
-connection = cx_Oracle.connect(user="hr", password="welcome",
-                               dsn="localhost/orclpdb1")
-
-cursor = connection.cursor()
-cursor.execute("""
-        SELECT first_name, last_name
-        FROM employees
-        WHERE department_id = :did AND employee_id > :eid""",
-        did = 50,
-        eid = 190)
-for fname, lname in cursor:
-    print("Values:", fname, lname)
+#This is the script for the automatic generation of the data of the linkedstats project for reuse within the INE. 
+import morph_kgc
+import sys
+input_data = sys.argv[1]
+client_dir = sys.argv[2]
+config = """
+        [CONFIGURATION]
+        oracle_client_lib_dir:"""+client_dir+"""
+        output_file: nt_data/clasificaciones_"""+input_data+"""_BD.nt
+        infer_sql_datatypes: yes
+        [DataSource1]
+        mappings: mappings/mappings_"""+input_data+""".ttl
+        db_url: oracle+cx_oracle://AYUDACOD2024PRO_READ:READAYUDACOD2024@http://p8dbejv12kvm.ine.es:1622/PRO19
+        """
+morph_kgc.materialize(config)
